87641c09ae91570f5fdd610dc42db15081a73538
Agung12 <115615712+agung-bot1@users.noreply.github.com>
2022-10-31 23:05:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -39,22 +39,22 @@ const readMore = more.repeat(4001)
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ NO OWNER ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
 
 global.owner = [
-['6285172446692', 'leviXd', true],
-['6282131369002', 'victor bot', true]
+['6285794583615', 'agung', true],
+['6285794583615', 'victor bot', true]
 ]
 global.mods = []
 global.prems = []
-global.nomorbot = '6282131369002'
-global.nomorown = '6285172446692'
-global.nomorown1 = '6285172446692'
-global.nomorown2 = '6282131369002'
+global.nomorbot = '6285794583615'
+global.nomorown = '6285794583615'
+global.nomorown1 = '6285794583615'
+global.nomorown2 = '6285794583615'
 global.nomorwa = '0'
 
 
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ WM ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
 
 global.readMore = readMore
-global.author = 'Created By leviXd'
+global.author = 'Agung'
 global.namebot = 'victor bot'
 global.wm = '© 𝘾𝙧𝙚𝙖𝙩𝙚𝙙 𝘽𝙮 victor md'
 global.media = 'https://telegra.ph/file/b69e2e87cdbaebb7751ce.jpg'
@@ -63,8 +63,8 @@ global.wm2 = 'victor bot '
 global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.packname = wm
 global.bottime = `T I M E : ${wktuwib}`
-global.titlebot = `victor 𝐗 leviXd`
-global.stickpack = 'victor 𝐗 leviXd'
+global.titlebot = `victor 𝐗 Agung`
+global.stickpack = 'victor 𝐗 Agung'
 global.stickauth = `© Victor MD`
 global.week = `${week} ${date}`
 global.wibb = `${wktuwib}`
@@ -84,12 +84,12 @@ global.sdc = '-' //discord
 global.snh = 'https://youtube.com/channel/UC2GOH5mCArh3E4J3sGFMC5g'
 
 //⫹⫺ ╳╶╼╶╶╶╶┈ ⎝ DONASI ⎞ ┈╴╴╴╴╾╴╳ ⫹⫺
-global.pdana = '085172446692'
+global.pdana = '085709420641'
 global.povo = '-'
-global.pgopay = '085172446692'
+global.pgopay = '-'
 global.plinkaja = '-' //link aja
-global.ppulsa = '0813 6128 1833'
-global.ppulsa2 = '083838186170'
+global.ppulsa = '0857 0942 0641'
+global.ppulsa2 = '-'
 global.psaweria = '-'
 
 
